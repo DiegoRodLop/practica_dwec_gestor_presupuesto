0fad72a5b5cf6033eead5afce7e8b7ce3aacbe0e
Diego Rodriguez <dierodlop@alu.edu.gva.es>
2024-11-10 20:10:25 +0100

    Subiendo las funciones de fundamentos js I

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,18 +2,41 @@
 
 // TODO: Variable global
 
+let presupuesto = 0.0;
 
 function actualizarPresupuesto() {
     // TODO
+
+    if(typeof nuevopresupuesto == "number" && nuevopresupuesto >= 0)
+    {
+        presupuesto = nuevopresupuesto;
+
+        console.log(presupuesto);
+        
+    }
+    else
+    {
+        alert("El valor introducido no es valido");
+    }
+
 }
 
 function mostrarPresupuesto() {
     // TODO
+
+    console.log(`Tu presupuesto es de ${presupuesto} €`);
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion, valor) {
     // TODO
+
+    this.descripcion = descripcion;
+    this.valor = (typeof valor == "number" && valor >= 0) ? valor : 0;
+
 }
+
+var gasto = new CrearGasto();
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
