1266c3d4608b5b57f34772938c59a219b0f4d6c3
Diego Rodriguez <dierodlop@alu.edu.gva.es>
2024-11-18 00:06:57 +0100

    Corrigiendo errores y añadiendo elementos a las funciones

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,8 +2,8 @@
 
 // TODO: Variable global
 
-var gastos;
-var idgasto;
+let gastos = [];
+let idgasto = 0;
 
 var presupuesto = 0.0;
 
@@ -34,12 +34,15 @@ function mostrarPresupuesto() {
     console.log(`Tu presupuesto es de ${presupuesto} €`);
 }
 
-function CrearGasto(descripcion, valor, fecha = [], etiqueta = []) {
+function CrearGasto(descripcion, valor, fecha = [], etiqueta = [], id) {
+    // El parametro id no estoy seguro de que se pase de esta forma, comprobaremos en la correción
     // TODO
 
     this.descripcion = descripcion;
     this.valor = (typeof valor == "number" && valor >= 0) ? valor : 0;
     this.etiqueta = Array.isArray(etiqueta) ? etiqueta : [];
+
+    id = anyadirGasto(); //llamamos la funcion anyadir gasto para que nos de la propiedad id
 
 
     //Consultado en chatgpt
@@ -50,8 +53,18 @@ function CrearGasto(descripcion, valor, fecha = [], etiqueta = []) {
         this.fecha = new Date();
     }
 
-    this.mostrarGasto = function() {
-        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    this.mostrarGastoCompleto = function() {
+
+        texto1 = `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`; 
+        texto2 = `Fecha: ${this.fecha.toLocaleString}`;
+        //Esta linea de codigo la he comprobado en chatgpt por que no estaba encontrando como meter el salto de linea y me ha puesto el tema de la fecha, yo me habia quedado en this.etiqueta.mas(etiqueta)
+        texto3 = `Etiquetas:
+        ${this.etiqueta.map(etiqueta => `- ${etiqueta}`).join("\n")}`;
+
+        //sumo todos los textos para no encandenarlos en el return y que este mas simple
+        texto4 = texto1 + texto2 + texto3;
+
+        return texto4
     }
 
     this.actualizarDescripcion = function(nuevaDescripcion) {
@@ -65,15 +78,16 @@ function CrearGasto(descripcion, valor, fecha = [], etiqueta = []) {
             console.error("Error: El valor introducido no es válido o es negativo.");
         }
     }
-}
 
-let gasto = {
-    descripcion:"",
-    valor:"",
-    fecha:"",
-    
+    this.actualizarfecha = function(){
 
-
+        // Intentar analizar la fecha
+        let timestamp = Date.parse(fecha);
+        if (!isNaN(timestamp)) {
+            // Si la fecha es válida, se actualiza la propiedad fecha
+            objeto.fecha = new Date(timestamp);
+        }
+    }
 }
 
 function listarGastos() {
@@ -84,24 +98,61 @@ function listarGastos() {
     return gastos;
 }
 
-function anyadirGasto() {
+function anyadirGasto(gasto) {
     //todo
 
-    id = id + idgasto;
+    // Ponemos en el parametro id el valor actual de la variable global idgasto
+    gasto.id = idgasto;
+    //incremento en  el valor de la variable idgasto
+    idgasto++;
 
-    return id;
+    //pasamos el objeto gasto a la variable global
+    gastos.push(gasto);
+
+    return id;    
 }
 
-function borrarGasto() {
+function borrarGasto(id) {
     //todo
+    
+    //cremoas variable eliminar que cogera la posicion donde se encuentra el gasto con el id introducidos
+    let eliminar = gastos.findIndex(gasto => gasto.id == id)
+
+    //si la posicion es distinta de -1 quiere decir que lo ha encontrado, por que nuestros objetos empiezan en 0
+    if(eliminar != -1){
+        //eliminamos la posicion donde se encuentra nuestro objeto y reorganizamos la lista
+        gastos.slice(eliminar, 1);
+    } 
 }
 
 function calcularTotalGastos() {
     //todo
+    //Creamos variable para almacenar la suma y poder devolverla
+
+    let suma = 0;
+
+    //Variable valor le pasamos el atributo valor de nuestros objeto
+    for(let valor of gastos.valor){
+        //Variable suma le vamos sumando el numero introducido en la variable valor
+        suma = suma + valor;
+    }
+
+    //Retornamos la suma
+    return suma
+
 }
 
 function calcularBalance() {
     //todo
+
+    //Creamos varible para almacenar la resta de los dos componentes
+    let Balance = 0;
+
+    //Realizamos la resta entre lo presupuesto y el calculo total de gastos, llamando a su funcion para no tener que volver a realizarla
+    Balance = presupeusto - calcularTotalGastos();
+
+    //Devolvemos la operación realizada
+    return Balance
 }
 
 
